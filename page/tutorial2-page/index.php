e6b1fa4fe88c6054c281cacbeab49495f67b1e04
lutherdev <devturd@gmail.com>
2025-05-25 19:33:01 +0800

    feat (tutorial page 2): added new div box for array tutorial

--- page/tutorial2-page/index.php
+++ page/tutorial2-page/index.php
@@ -25,6 +25,27 @@ navHeader($navList);
                     include __DIR__ . '/../../components/templates/codeBox.component.php';
                 ?>
             </div>
+            <div class="box">
+                <?php
+                    $title = $boxData[4]["title"];
+                    $description = $boxData[4]["description"];
+                    $scenario = $boxData[4]["scenario"];
+                    include __DIR__ . '/../../components/templates/textBox.component.php';
+                    $snippet = $boxData[4]["snippet"];
+                    $funcName = $boxData[4]["funcName"];
+                    $num = 1;
+                    ob_start();
+                    $newStuds = [];
+                    foreach ($arrayStuds as $itStud){
+                        $newStuds[$itStud] = 300; 
+                    }
+                    foreach ($newStuds as $itStud => $money){
+                        echo "User: $itStud, Balance: $money <br>";
+                    }
+                    $outputHtml = ob_get_clean();
+                    include __DIR__ . '/../../components/templates/codeBox.component.php';
+                ?>
+            </div>
             <div class ="pageButton">
                 <a href="../tutorial-page/index.php">BACK</a>
                 <a href="../tutorial3-page/index.php">NEXT</a>
