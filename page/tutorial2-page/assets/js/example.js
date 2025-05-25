956cd6360fb3dcb6cfd283f81822086e701d1f47
lutherdev <devturd@gmail.com>
2025-05-25 19:34:02 +0800

    feat: added a function for outputbox1

--- page/tutorial2-page/assets/js/example.js
+++ page/tutorial2-page/assets/js/example.js
@@ -2,3 +2,8 @@ function showOutputBox0(){
         document.getElementById("outputBox0").classList.toggle("visible");
         console.log("ITS CLICKING");
     }
+
+function showOutputBox1(){
+        document.getElementById("outputBox1").classList.toggle("visible");
+        console.log("ITS CLICKING");
+    }
