05a0d1ac7c8a3432c21dd09c1d830b986e6787b6
lutherdev <devturd@gmail.com>
2025-05-25 16:09:33 +0800

    feat (tutorial page 3): added initial boxes for output

--- page/tutorial3-page/index.php
+++ page/tutorial3-page/index.php
@@ -15,16 +15,49 @@ navHeader($navList);
                     <h1>PREDEFINED FUNCTIONS!</h1>
                     <p>"PREDEFINED" meaning the built in functions of PHP, and here some category to use it on</p>
                     <p>String Manipulation, Number Manipulation, and Date Manipulation</p>
+                    <p>Scenario: An array with names with their final grade, and has to determine the highest grade for them to win a random item!</p>
+                    <p>$studs = ['Lelouch' => '95', 'Ayanokoji' => '99', 'Zenitsu' => '84', 'Light' => '93', 'Shikamaru' => '95']</p>
                 </div>
             </div>
-            <div class="box"> //TODO create a way to show each box
+            <!-- TODO create a way to show each box -->
+            <div class="box"> 
                 <?php
-                    $title = $boxData[3]["title"];
-                    $description = $boxData[3]["description"];
-                    $scenario = $boxData[3]["scenario"];
+                    $title = $boxData[4]["title"];
+                    $description = $boxData[4]["description"];
+                    $scenario = $boxData[4]["scenario"];
                     include __DIR__ . '/../../components/templates/textBox.component.php';
-                    $snippet = $boxData[3]["snippet"];
-                    $funcName = $boxData[3]["funcName"];
+                    $snippet = $boxData[4]["snippet"];
+                    $funcName = $boxData[4]["funcName"];
+                    $num = 0;
+                    ob_start();
+                    capitalizeFLetter($arrayStuds);
+                    $outputHtml = ob_get_clean();
+                    include __DIR__ . '/../../components/templates/codeBox.component.php';
+                ?>
+            </div>
+            <div class="box"> 
+                <?php
+                    $title = $boxData[5]["title"];
+                    $description = $boxData[5]["description"];
+                    $scenario = $boxData[5]["scenario"];
+                    include __DIR__ . '/../../components/templates/textBox.component.php';
+                    $snippet = $boxData[5]["snippet"];
+                    $funcName = $boxData[5]["funcName"];
+                    $num = 0;
+                    ob_start();
+                    capitalizeFLetter($arrayStuds);
+                    $outputHtml = ob_get_clean();
+                    include __DIR__ . '/../../components/templates/codeBox.component.php';
+                ?>
+            </div>
+            <div class="box"> 
+                <?php
+                    $title = $boxData[4]["title"];
+                    $description = $boxData[4]["description"];
+                    $scenario = $boxData[4]["scenario"];
+                    include __DIR__ . '/../../components/templates/textBox.component.php';
+                    $snippet = $boxData[4]["snippet"];
+                    $funcName = $boxData[4]["funcName"];
                     $num = 0;
                     ob_start();
                     capitalizeFLetter($arrayStuds);
