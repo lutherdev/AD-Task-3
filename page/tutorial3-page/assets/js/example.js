94c542dd4f3eafd7c4523d4fecb6a1422a8f5672
lutherdev <devturd@gmail.com>
2025-05-25 16:55:34 +0800

    feat (tutorial page 3): added functions for each box in tutorial page 3

--- page/tutorial3-page/assets/js/example.js
+++ page/tutorial3-page/assets/js/example.js
@@ -2,3 +2,11 @@ function showOutputBox0(){
         document.getElementById("outputBox0").classList.toggle("visible");
         console.log("ITS CLICKING");
     }
+function showOutputBox1(){
+        document.getElementById("outputBox1").classList.toggle("visible");
+        console.log("ITS CLICKING");
+    }
+function showOutputBox2(){
+        document.getElementById("outputBox2").classList.toggle("visible");
+        console.log("ITS CLICKING");
+    }
