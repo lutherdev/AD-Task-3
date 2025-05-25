35ce7cf8b2de0c16352d3b61175e2c30370a0ddc
lutherdev <devturd@gmail.com>
2025-05-25 19:50:19 +0800

    feat (congrats page): added a new page for congratulating the programmer

---
+++ page/congrats-page/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/../../utils/capitalizeFLetter.utils.php';
+require_once __DIR__ . '/../../utils/getRandId.utils.php';
+require_once __DIR__ . '/../../handlers/pageData.handler.php';
+require_once __DIR__ . '/../../components/head.component.php';
+require_once __DIR__ . '/../../components/nav.component.php';
+?>
+
+<?php
+head("LEVEL 3", "tutorial3");
+navHeader($navList);
+?>
+        <div id="main">
+            <div class="box">
+                <div id="hero-text">
+                    <h1>CONGRATULATIONS PROGRAMMER!</h1>
+                    <p><span>YOU</span> now know the basics of PHP! <br> #GetCertifiedPHP</p>
+                </div>
+            </div>
+            <div class ="pageButton">
+                <a href="../tutorial2-page/index.php">BACK</a>
+                <a href="/index.php">BACK TO HOME</a>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/example.js"></script>
+</body>
+</html>
+
